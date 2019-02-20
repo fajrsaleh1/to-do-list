349ddb7c46a37edd6df18cb11db4bffdf9cca2fd
fajrsaleh1 <fajrsalbakiri@gmail.com>
2019-02-20 16:11:20 +0300

    Updates

---
+++ precache-manifest.be384784789c10eaf9405f3186915689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64ff023f16456c218c45",
+    "url": "/to-do-list/static/css/main.c1c8d8b2.chunk.css"
+  },
+  {
+    "revision": "64ff023f16456c218c45",
+    "url": "/to-do-list/static/js/main.64ff023f.chunk.js"
+  },
+  {
+    "revision": "9d55d607ae1fdfc75248",
+    "url": "/to-do-list/static/js/runtime~main.9d55d607.js"
+  },
+  {
+    "revision": "b2f92ae109246bc19d23",
+    "url": "/to-do-list/static/js/2.b2f92ae1.chunk.js"
+  },
+  {
+    "revision": "1ccf2939be5eabfe5e0e8fe24cc72a2d",
+    "url": "/to-do-list/index.html"
+  }
+];
